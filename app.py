8034902b563ec35103f867dd89e619c4204cace1
eloynav <81321529+eloynav@users.noreply.github.com>
2022-11-30 15:36:08 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    vmessage = "Hello World! Today is: "+date_time 
+    return vmessage
+
+if __name__ == '__main__':
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')

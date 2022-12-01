1a05ed5938300fbae155658dca1878d05a2d121c
eloynav <81321529+eloynav@users.noreply.github.com>
2022-12-01 09:25:39 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from datetime import datetime
+from tkinter import *
 import os
 
 app = Flask(__name__)
@@ -9,6 +10,17 @@ def hello():
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
     vmessage = "Hello World! Today is: "+date_time 
+
+    root = Tk()
+    var = IntVar()
+    R1 = Radiobutton(root, text = "HOG Application Summary", variable=var, value=1, command=sel)
+    R1.pack(anchor = W)
+    R2 = Radiobutton(root, text = "HOG Provincial Summary",  variable=var, value=2, command=sel)
+    R2.pack(anchor = W)
+    label = Label(root)
+    label.pack()
+    root.mainloop()
+
     return vmessage
 
 if __name__ == '__main__':
